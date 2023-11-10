da41224c0d1a62ac64f4f9b5fa72c0ad5a9fcadf
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-11-10 16:13:17 +0100

    app.js debug(wip)

--- le-12-fatiche-di-javascript/app.js
+++ le-12-fatiche-di-javascript/app.js
@@ -1,27 +1,27 @@
-const input = document.querySelector('input');
-const array = null;
+const input = document.querySelector('.input'); /* manca il punto del selettore CSS*/
+const array = []; /* null e' un valore in se e non e' un array*/
 
 input.addEventListener('keypress', function(event) {
 
     if (event.code != 'Enter')    return;
     if (input.value.length == '') return;
 
-    array.add(input.value);
+    array.push(input.value); /* forse era push e non add(che non conosco)*/
 
     const li = document.createElement('li');
     li.classList.add('list-group-item');
-    li.text = input.value;
-    document.querySelector('ul').push(li);
+    li.textContent = input.value; /* textContent e non text*/
+    document.querySelector('ul').append(li); /* penso sia append, penso */
 
     let counter = '';
     let item    = array[0];
     const max   = 1;
     const elems = [];
 
-    for (let i = 0; i < array; i++) {
+    for (let i = 0; i < array.length; i++) { /* array.length e non array e basta */
         let val = array[i];
 
-        if (!elems[val]) {
+        if (!elems[val]) { /* per forza false elems non ha elementi*/
             elems[val] = 1;
         } else {
             elems[val]++;
